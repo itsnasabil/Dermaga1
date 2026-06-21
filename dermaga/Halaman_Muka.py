3d401ef2ff7f290ac32c62319027f79ee37f47b8
lalalaa <152621466+itsnasabil@users.noreply.github.com>
2026-06-21 16:39:24 +0700

    Change Google site verification meta tag
    Updated Google site verification meta tag in Halaman_Muka.py.

--- dermaga/Halaman_Muka.py
+++ dermaga/Halaman_Muka.py
@@ -1,12 +1,11 @@
 import streamlit as st
+st.markdown(
+    '<meta name="google-site-verification" content="ISI_TOKEN_DARI_GOOGLE_DI_SINI" />',
+    unsafe_allow_html=True
+)
 from utils import set_page_config, render_header, render_footer
 from assets import LOGO_DERMAGA_B64
 
-
-st.markdown(
-    '<meta name="google-site-verification" content="ttRf-6fP_HKFr5CiFzOukh_ibe-x5pReEmNwvau6ung" />',
-    unsafe_allow_html=True
-)
 
 set_page_config()
 
