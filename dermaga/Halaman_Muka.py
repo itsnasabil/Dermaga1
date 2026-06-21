4a1160f0c5afe9b61df009671f90b3c00f41c23a
lalalaa <152621466+itsnasabil@users.noreply.github.com>
2026-06-21 16:41:32 +0700

    Update Google site verification meta tag

--- dermaga/Halaman_Muka.py
+++ dermaga/Halaman_Muka.py
@@ -1,12 +1,8 @@
 import streamlit as st
-st.markdown(
-    '<meta name="google-site-verification" content="ISI_TOKEN_DARI_GOOGLE_DI_SINI" />',
-    unsafe_allow_html=True
-)
 from utils import set_page_config, render_header, render_footer
 from assets import LOGO_DERMAGA_B64
 
-
+<meta name="google-site-verification" content="ttRf-6fP_HKFr5CiFzOukh_ibe-x5pReEmNwvau6ung" />
 set_page_config()
 
 # ── PAGE CSS ─────────────────────────────────────────────────────────────────
